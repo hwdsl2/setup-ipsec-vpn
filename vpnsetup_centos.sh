536ac8f54b3cc8682f56a0ade828429284d5beb6
hwdsl2 <hwdsl2@users.noreply.github.com>
2017-09-27 21:41:24 -0500

    Update ipsec.conf
    - Replace obsolete keyword "virtual_private" with "virtual-private"

--- vpnsetup_centos.sh
+++ vpnsetup_centos.sh
@@ -213,7 +213,7 @@ cat > /etc/ipsec.conf <<EOF
 version 2.0
 
 config setup
-  virtual_private=%v4:10.0.0.0/8,%v4:192.168.0.0/16,%v4:172.16.0.0/12,%v4:!$L2TP_NET,%v4:!$XAUTH_NET
+  virtual-private=%v4:10.0.0.0/8,%v4:192.168.0.0/16,%v4:172.16.0.0/12,%v4:!$L2TP_NET,%v4:!$XAUTH_NET
   protostack=netkey
   nhelpers=0
   interfaces=%defaultroute
