47e1c9205166099beacfcbeda5fa52ebc07490aa
hwdsl2 <hwdsl2@users.noreply.github.com>
2017-10-26 01:37:35 -0500

    Clean up ipsec.conf
    - Remove unneeded option nhelpers=0

--- vpnsetup.sh
+++ vpnsetup.sh
@@ -222,7 +222,6 @@ version 2.0
 config setup
   virtual-private=%v4:10.0.0.0/8,%v4:192.168.0.0/16,%v4:172.16.0.0/12,%v4:!$L2TP_NET,%v4:!$XAUTH_NET
   protostack=netkey
-  nhelpers=0
   interfaces=%defaultroute
   uniqueids=no
 
