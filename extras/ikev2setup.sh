dce25ada28ea8cd5d1a931ba1bcccb797a8e537e
hwdsl2 <hwdsl2@users.noreply.github.com>
2022-05-23 08:22:53 -0500

    Update IKEv2 script
    - Add a note about OpenVPN and WireGuard which can be optionally
      installed alongside IPsec VPN.

--- extras/ikev2setup.sh
+++ extras/ikev2setup.sh
@@ -151,7 +151,7 @@ confirm_or_abort() {
 show_header() {
 cat <<'EOF'
 
-IKEv2 Script   Copyright (c) 2020-2022 Lin Song   14 May 2022
+IKEv2 Script   Copyright (c) 2020-2022 Lin Song   23 May 2022
 
 EOF
 }
@@ -1278,6 +1278,20 @@ vpnsetup.net/ikev2clients
 EOF
 }
 
+show_optional_note() {
+  if [ "$os_type" = "ubuntu" ] || [ "$os_type" = "debian" ] \
+    || { [ "$os_type" = "centos" ] && [ "$os_ver" = "7" ]; }; then
+cat <<EOF
+Note: You may optionally install OpenVPN and/or
+WireGuard alongside IPsec VPN on this server.
+Learn more at vpnsetup.net/ovpn vpnsetup.net/wg
+
+================================================
+
+EOF
+  fi
+}
+
 check_swan_update() {
   base_url="https://github.com/hwdsl2/vpn-extras/releases/download/v1.0.0"
   swan_ver_url="$base_url/upg-$os_type-$os_ver-swanver"
@@ -1611,6 +1625,7 @@ ikev2setup() {
   print_setup_complete
   print_client_info
   if [ "$in_container" = "0" ]; then
+    show_optional_note
     check_swan_update
   fi
 }
