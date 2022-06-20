39bdb36484ab814c017da79cd3606951e45d3316
hwdsl2 <hwdsl2@users.noreply.github.com>
2022-06-19 22:17:13 -0500

    Update IKEv2 script
    - Add Alpine Linux 3.16 and remove 3.14.

--- extras/ikev2setup.sh
+++ extras/ikev2setup.sh
@@ -90,8 +90,8 @@ EOF
     esac
     if [ "$os_type" = "alpine" ]; then
       os_ver=$(. /etc/os-release && printf '%s' "$VERSION_ID" | cut -d '.' -f 1,2)
-      if [ "$os_ver" != "3.14" ] && [ "$os_ver" != "3.15" ]; then
-        exiterr "This script only supports Alpine Linux 3.14/3.15."
+      if [ "$os_ver" != "3.15" ] && [ "$os_ver" != "3.16" ]; then
+        exiterr "This script only supports Alpine Linux 3.15/3.16."
       fi
     else
       os_ver=$(sed 's/\..*//' /etc/debian_version | tr -dc 'A-Za-z0-9')
