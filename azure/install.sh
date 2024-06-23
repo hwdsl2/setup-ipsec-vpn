8339f7253fbcef3c90f6b67e5efefc53d645a55e
hwdsl2 <hwdsl2@users.noreply.github.com>
2024-06-23 18:46:24 -0500

    Update Azure script
    - Minor fix

--- azure/install.sh
+++ azure/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-export VPN_IPSEC_PSK=$1
-export VPN_USER=$2
-export VPN_PASSWORD=$3
+export VPN_IPSEC_PSK="$1"
+export VPN_USER="$2"
+export VPN_PASSWORD="$3"
 
 wget -t 3 -T 30 -nv -O vpn.sh https://raw.githubusercontent.com/hwdsl2/setup-ipsec-vpn/master/vpnsetup.sh && sh vpn.sh
