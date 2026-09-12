b91854ce5642dd1e6d2dbd3989029b37e83201c2
hwdsl2 <hwdsl2@users.noreply.github.com>
2026-09-12 00:49:40 -0500

    Harden Azure installer
    Pin vpnsetup.sh to an immutable revision and verify its SHA-256
    checksum before execution.
    
    Thanks to @Tzswz962 for reporting the unverified Azure bootstrap
    download.

--- azure/install.sh
+++ azure/install.sh
@@ -4,4 +4,19 @@ export VPN_IPSEC_PSK="$1"
 export VPN_USER="$2"
 export VPN_PASSWORD="$3"
 
-wget -t 3 -T 30 -nv -O vpn.sh https://raw.githubusercontent.com/hwdsl2/setup-ipsec-vpn/master/vpnsetup.sh && sh vpn.sh
+VPN_SETUP_REF="ba2b765b5e9a05f89aaf56f3c8a326399674bcdf"
+VPN_SETUP_SHA256="b2559dd6b2dd6ca80823db3a9efd9c3560d78554caa573b635b8bcbd4a903708"
+VPN_SETUP_URL="https://raw.githubusercontent.com/hwdsl2/setup-ipsec-vpn/$VPN_SETUP_REF/vpnsetup.sh"
+
+umask 077
+tmp_file=$(mktemp /tmp/vpnsetup.XXXXXX) || exit 1
+trap 'rm -f "$tmp_file"' 0
+trap 'exit 1' HUP INT TERM
+
+wget -t 3 -T 30 -nv --max-redirect=0 -O "$tmp_file" "$VPN_SETUP_URL" || exit 1
+
+printf '%s  %s\n' "$VPN_SETUP_SHA256" "$tmp_file" | sha256sum -c - || exit 1
+
+/bin/sh "$tmp_file"
+status=$?
+exit "$status"
